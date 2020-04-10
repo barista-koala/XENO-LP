ccdbf70bfce8266f7f3f4034079525bc24f9c2df
ユーザー名 <[ance12tajadrl@docomo.ne.jp]>
2020-04-10 20:50:24 +0900

    update

--- animation.js
+++ animation.js
@@ -31,6 +31,17 @@ $(window).scroll(function() {
     });
 });
 
+$(window).scroll(function() {
+    $('.last').each(function() {
+        var elemPos = $(this).offset().top,
+            scroll = $(window).scrollTop(),
+            windowHeight = $(window).height();
+        if (scroll > elemPos - windowHeight + 100) {
+            $(this).addClass('scrollin');
+        }
+    });
+});
+
 $(function() {
     $('a[href*="#"].anchor-link').on('click', function() {
         var speed = 400;
